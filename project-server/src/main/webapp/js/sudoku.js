d2e6f545b3291da6ecbff15eb87980b9b7ddac48
Mirko <mirkomueller97@live.de>
2017-06-14 12:11:14 +0200

    a (#16)
    * Split index from sudoku
    
    * removed leftover from merge
    
    * added sudoku css & js
    
    * added messageBox + numCheck
    
    * fixed input bugs

--- project-server/src/main/webapp/js/sudoku.js
+++ project-server/src/main/webapp/js/sudoku.js
@@ -3,18 +3,31 @@ $(document).ready(function() {
 });
 	
 function tdClickHandler(e){
-	var input = document.createElement('input');
-	input.id = "inputField";
-	e.target.innerHTML = "";
-	e.target.append(input);
-	$('#inputField').keyup(inputKeyUpHandler);
+	e.stopPropagation()
+	if (e.target.id != 'inputField') {
+		$('#inputField').remove()
+		var input = document.createElement('input');
+		input.id = "inputField";
+		e.target.innerHTML = "";
+		e.target.append(input);
+		$('#inputField').unbind('keyup');
+		$('#inputField').keyup(inputKeyUpHandler);
+	}
 }
 function inputKeyUpHandler(e){
 	var key = e.which;
 	if(key == 13)  // the enter key code
 	  {
 		var test = $('#inputField').val();
-		$(this).parent().html(test);
-		$('#inputField').remove();
+		if(parseInt(test) < 10 && parseInt(test) > 0 ){
+			$(this).parent().html(test);
+			$('#inputField').remove();
+		} else {
+			writeToMessageBoard(test + " is not a valid number...")
+		}
 	  }
+}
+function writeToMessageBoard(m){
+	$('#messageBoard').append(document.createTextNode( "\n"+m ))
+	$('#messageBoard').scrollTop($('#messageBoard')[0].scrollHeight);
 }
