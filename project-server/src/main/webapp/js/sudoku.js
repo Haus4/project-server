b7bfff493153d5ac8b515a37ee2cbe9194bf6382
momo5502 <mauriceheumann@gmail.com>
2017-06-20 09:28:57 +0200

    Add styling to the sudoku page

--- project-server/src/main/webapp/js/sudoku.js
+++ project-server/src/main/webapp/js/sudoku.js
@@ -1,39 +1,38 @@
-$(document).ready(function() {
+$(document).ready(function () {
 	$("td[class='active']").click(tdClickHandler);
 });
-	
-function tdClickHandler(e){
-	e.stopPropagation()
+
+function tdClickHandler(e) {
+	e.stopPropagation();
+
 	if (e.target.id != 'inputField') {
 		$('#inputField').remove();
+
 		var input = document.createElement('input');
 		input.id = "inputField";
 		e.target.innerHTML = "";
 		$(this).append(input);
+
 		$('#inputField').unbind('keyup');
 		$('#inputField').keyup(inputKeyUpHandler);
 	}
 }
-function inputKeyUpHandler(e){
-	var key = e.which;
-	if(key == 13)  // the enter key code
-	  {
+
+function inputKeyUpHandler(e) {
+	if (e.which == 13)  // the enter key code
+	{
 		var test = $('#inputField').val();
-		if(parseInt(test) < 10 && parseInt(test) > 0 ){
+		if (parseInt(test) < 10 && parseInt(test) > 0) {
 			var id = $(this).parent().get(0).id;
 			sendResult(parseInt(test), id);
+
 			$(this).parent().html(test);
 			$('#inputField').remove();
-		} else {
-			writeToMessageBoard(test + " is not a valid number...");
 		}
-	  }
+	}
 }
-function writeToMessageBoard(m){
-	$('#messageBoard').append(document.createTextNode( "\n"+m ));
-	$('#messageBoard').scrollTop($('#messageBoard')[0].scrollHeight);
-}
-function sendResult(v, i){
+
+function sendResult(v, i) {
 	console.log({
 		id: i,
 		value: v
